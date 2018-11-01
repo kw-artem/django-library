669c4c7bcc872a8cc02c4ef021593bcaa0c889fc
kw-artem <kw-artem@yandex.ru>
2018-11-01 09:33:47 +0300

    Sessions. Added simple page visitors counter.

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import django.contrib.sessions
 from .models import Book, Author, BookInstance, Genre
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -10,10 +11,13 @@ def index(request):
 	num_instances_available=BookInstance.objects.filter(status__exact='a').count()
 	num_authors = Author.objects.all().count()
 	
+	num_visits = request.session.get('num_visits', 0)
+	request.session['num_visits'] = num_visits + 1
+	
 	return render(
 		request,
 		'index.html',
-		context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+		context={'num_books':num_books, 'num_instances':num_instances, 'num_instances_available':num_instances_available, 'num_authors':num_authors, 'num_visits':num_visits},
 	)
 
 
